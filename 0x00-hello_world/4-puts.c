452c5a553f04061443423e4f4bd616d18eb4367b
shrook taher <shrooktaher@yahoo.com>
2023-06-15 12:41:13 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
+ * Description: A C program that prints printf function.
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }	
