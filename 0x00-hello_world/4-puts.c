2fee66b6cadc8029c2bca6b943a523f16337961e
shrook taher <shrooktaher@yahoo.com>
2023-06-15 04:00:20 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints put function
+ *
+ * Returns: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}		
