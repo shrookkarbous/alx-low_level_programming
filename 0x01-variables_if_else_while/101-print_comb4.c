4da22933bd3ae9552bc78d14df33ff553508d7ad
shrook taher <shrooktaher@yahoo.com>
2023-06-18 13:03:39 -0700

    e

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,8 @@
 /**
  * main: Entry point
  *
- * Description: print all possible different combination of three digits
+ * Description: print all possible different combination
+ *		of three digits
  *
  * Return: Always 0 (Success)
 */
