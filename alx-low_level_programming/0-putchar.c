7e584e732c10435f6626f9d9f1c979246e75b9e0
shrook taher <shrooktaher@yahoo.com>
2023-06-22 05:58:58 -0700

    z

---
+++ alx-low_level_programming/0-putchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A Programm that prints _putchar
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char me[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+	{
+		printf("%c\n, me");
+	}
+
+	return (0);
+}
