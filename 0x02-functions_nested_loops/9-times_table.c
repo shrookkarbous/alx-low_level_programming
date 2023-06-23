61528cc4f6c54b9dee7e71ef15f3f4374add7ccd
shrook taher <shrooktaher@yahoo.com>
2023-06-23 06:14:18 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -32,11 +32,11 @@ void times_table(void)
 			if (prod <= 9)
 			{
 				_putchar(' ');
-			else
+			} else if
 			{
 				_putchar((prod / 10) + 48); /*get the first digit*/
 				_putchar((prod % 10) + 48); /*get the second digit*/
-		
+			}
 		}
 		_putchar('\n');
 	}
