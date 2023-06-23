ceced7968ffd9b8778ee10a3d01487fce84fbe84
shrook taher <shrooktaher@yahoo.com>
2023-06-23 12:33:37 -0700

    z

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,26 @@
 #include"main.h"
 
 /**
- * _sqrt - */
+ * print_number - prints an integer
+ *
+ * @n: the integer to be printed
+*/
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	/*first check if its negative*/
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	/*print the first few digits*/
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	/*print the last digit*/
+	_putchar((num % 10) + 48);
+}
