85692b1c63630280b308a6576cc2e9a15097f85a
shrook taher <shrooktaher@yahoo.com>
2023-06-23 03:51:28 -0700

    a

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  A Function prints every minute of the day
+*/
+
+void jach_bauer(void)
+{
+	int hr;
+	int min;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
