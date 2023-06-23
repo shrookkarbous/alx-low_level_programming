ddc3fec5cb74ee4a6c1f69100a822fe42ac2e774
shrook taher <shrooktaher@yahoo.com>
2023-06-23 10:15:53 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,18 @@
 
 void print_diagonal(int n)
 {
-	int postn, space;
+	int postn;
+	int space;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (space = 1; space <= n; space++)
-			-putchar(' ');
-		_putchar(92);
-		_putchar('\n');
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (space = 1; space <= posten; space++)
+				-putchar(' ');
+			_putchar(92);
+			_putchar('\n');
 	}
 }
