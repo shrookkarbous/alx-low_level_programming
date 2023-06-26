ab44d52d47decf0a7f4a05b22d90533bdeed0758
shrook taher <shrooktaher@yahoo.com>
2023-06-26 02:25:07 -0700

    s

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * _puts -  function that prints a string,
+ *		followed by a new line, to stdout
+ *
+ * @str: char paramter
+ *
+ * Return: void
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_puts(*str + 0);
+		str++;
+	}
+	_puts('\n');
+}
