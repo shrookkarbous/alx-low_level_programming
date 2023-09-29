f15ad404f8c612e4b0d1433d65a7dfe90d2d5d5c
Shrook Karbous <shrooktaher@yahoo.com>
2023-09-29 11:24:56 -0700

    r

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * is_prime_number -  a function that returns 1 if
+ * the input integer is a prime number, otherwise return 0
+ * @n: integer
+ * @m: integer
+ * Return: int
+ */
+
+int priming(int n, int m);
+int is_prime_number(int n)
+{
+	return (priming(n, 2));
+}
+
+/**
+ * priming - is a function
+ * @n: integer
+ * @m: integer
+ * Return: int
+ */
+
+int priming(int n, int m)
+{
+	if (m >= n && n > 1)
+		return (1);
+	else if (n % m == 0 || n <= 1)
+		return (0);
+	else
+		return (priming(n, m + 1));
+}
