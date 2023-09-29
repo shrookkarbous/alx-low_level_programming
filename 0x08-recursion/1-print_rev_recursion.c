69e0d8f1172a26cf808f0f5fab2e746add22d2c8
Shrook Karbous <shrooktaher@yahoo.com>
2023-09-29 01:12:34 -0700

    r

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - function prints a string in reverse
+ * @s: pointer to a srting
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+	_print_rev_recursion(s + 1);
+	putchar(s);
+	}
+}
+
