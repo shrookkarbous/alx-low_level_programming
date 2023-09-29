cdafbecf426d6352d4b46cfc28cc21986b1bba1e
Shrook Karbous <shrooktaher@yahoo.com>
2023-09-29 00:28:37 -0700

    r

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * _puts_recursion - function print a string followed
+ * by a new line
+ * @s: pointer to a string
+ * Return: void
+ */
+
+void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		puts('\n');
+	}
+	else
+	{
+		puts(*s);
+		_puts_recursion(s + 1);
+	}
+}
