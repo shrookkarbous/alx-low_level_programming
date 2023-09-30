aec0dfb8bf9bb6bcb6f1246bf742d29aae4950f5
shrook karbous <shrooktaher@yahoo.com>
2023-09-30 13:14:38 -0700

    a

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,25 +1,36 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * is_palindrome - a function that returns 1 if
- * a string is a palindrome and 0 if not.
- * @s: pointer to a string
- * Return: int
- */
-
 int search(char *s, int begain, int end, int mod);
 int last_index(char *s);
+
+/**
+ * last_index - a function
+ * @*s: pointer to a string
+ * Return: int
+ */
 
 int last_index(char *s)
 {
 	int n = 0;
-	
-	if (s* > '\0')
-		n += last_index(s+1) + 1;
+
+	if (*s > '\0')
+		n += last_index(s + 1) + 1;
 
 	return (n);
 }
+
+
+/**
+ * is_palindrome - a function that returns 1 if
+ * a string is a palindrome and 0 if not.
+ * last_index - a function searches the last index
+ * @begain: int
+ * @end: int
+ * @mod: int
+ * @*s: pointer to a string
+ * Return: int
+ */
 
 int is_palindrome(char *s)
 {
@@ -29,7 +40,7 @@ int is_palindrome(char *s)
 }
 
 /**
- * search - function searching the plaindrom
+ * search - function searching for plaindrom
  * @*s: pointer to a string
  * @begain: int the begaining
  * @end: int the end
@@ -44,5 +55,5 @@ int search(char *s, int begain, int end, int mod)
 	else if (s[begain] != s[end])
 		return (0);
 	else
-		return (search(s, begain + 1, end - 1, mod);
+		return (search(s, begain + 1, end - 1, mod));
 }
