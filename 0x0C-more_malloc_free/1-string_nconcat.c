bc9ef551394b1fb4786302ac5fa070844b10eef7
Shrook Karbous <shrooktaher@yahoo.com>
2023-10-07 11:24:08 -0700

    w

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,14 +13,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *c;
 	int r, m, sl1, sl2;
 
-	/*check if the string passed is NULL*/
-
 	if (s1 == 0)
 		s1 = "";
 	if (s2 == 0)
 		s2 = "";
-
-	/*calculate the length of the string*/
 
 	for (sl1 = 0; s1[sl1] != '\0'; sl1++)
 		;
@@ -28,18 +24,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (sl2 = 0; s2[sl2] != '\0'; sl2++)
 		;
 
-	/*memory reservation*/
 	c = malloc(sl1 + n + 1);
 	if (c == NULL)
 	{
 		return (NULL);
 	}
 
-	/*copy first string to c*/
 	for (r = 0; s1[r] != '\0'; r++)
 		c[r] = s1[r];
 
-	/*copy second string to c*/
 	for (m = 0; m < n; m++)
 	{
 		c[r] = s2[m];
