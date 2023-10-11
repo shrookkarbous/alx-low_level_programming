6aba0f098c3ffb6ec3e8be287a5f2aac9c066a90
Shrook Karbous <shrooktaher@yahoo.com>
2023-10-11 12:10:38 -0700

    w

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog 
+ * @d: the dog to be intialized
+ * @name: string
+ * @age: integer
+ * @owner: string
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
