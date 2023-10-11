73db35eeac397bb0cf0f48950a771334dde5cbfd
Shrook Karbous <shrooktaher@yahoo.com>
2023-10-11 12:02:25 -0700

    w

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: string
+ * @age: integer
+ * @owner: string
+ *
+ * Description: alone Dog struc in a big kitty world
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner
+};
+
+#endif
