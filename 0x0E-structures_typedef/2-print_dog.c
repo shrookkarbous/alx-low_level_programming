49d39d1afc31598295f75c0e04f661b029587f87
Shrook Karbous <shrooktaher@yahoo.com>
2023-10-11 12:38:51 -0700

    w

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,12 +7,13 @@
  *
  * Return: void
  */
+i
 void print_dog(struct dog *d)
 {
 	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner : "(nil)");
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
