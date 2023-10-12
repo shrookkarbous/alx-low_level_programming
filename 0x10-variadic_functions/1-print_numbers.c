4ad90183d3de5ce90a5e25ab1dd4b393b148d32a
Shrook Karbous <shrooktaher@yahoo.com>
2023-10-12 11:37:20 -0700

    w

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 	va_start(ap, n);
 	while (i--)
-		printf("%d", va_arg(ap, int),
+		printf("%d%s", va_arg(ap, int),
 				i ? (separator ? separator : "") : "\n");
 	va_end(ap);
 }
