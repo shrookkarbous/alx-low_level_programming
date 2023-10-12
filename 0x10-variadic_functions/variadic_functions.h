dbd74ce0aed4c54990565477a5136459b204e1e7
Shrook Karbous <shrooktaher@yahoo.com>
2023-10-12 12:20:35 -0700

    w

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -21,5 +21,8 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void format_char(char *separator, va_list ap);
+void format_int(char *separator, va_list ap);
+void format_string(char *separator, va_list ap);
 
 #endif
