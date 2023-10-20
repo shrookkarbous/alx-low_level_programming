c96e823b31156222ef5d7eac7519ef2595202fa1
Shrook Karbous <shrooktaher@yahoo.com>
2023-10-20 15:33:43 -0700

    w

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements of a list_t list
+ * h: pointer to the elements of list_t
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+        int n, number_of_nodes;
+
+        list_t *h{
+                char data;
+                list_t *h;
+        };
+
+        if (h->data != 'NULL')
+        {
+                printf("%c", data);
+                list_t *h;
+        }
+        else if
+        {
+                printf("[0]");
+        }
+
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,10 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+size_t print_list(const list_t *h);
+
+#endif
