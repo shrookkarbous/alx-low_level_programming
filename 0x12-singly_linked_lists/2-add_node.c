e7480037bc633ba5b297721b1d24919560866f35
Shrook Karbous <shrooktaher@yahoo.com>
2023-10-21 10:51:05 -0700

    w

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_node - a function that adds a new node at the beginning of a list_t list
+ * @head: pointer to struct
+ * @str: pointer to a string
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	next = &head;
+
+	while (head)
+	{
+		if (head->next)
+		{
+			printf("%s\n", next->str);
+		}
+		else if
+		{
+			return (NULL);
+		}
+	}
+	return (&head);
+}
