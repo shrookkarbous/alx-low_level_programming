9b892650cedc0f022da6cdc1383937c1d2204552
Shrook Karbous <shrooktaher@yahoo.com>
2023-10-21 12:56:29 -0700

    w

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,16 +8,17 @@
 
 void free_list(list_t *head)
 {
-	list_t *node;
+	list_t *node, *next_node;
 
 	if (!head)
 		return;
 
+	node = head;
 	while (head)
 	{
-		node = head->next;
+		next_node = node->next;
 		free(node->str);
 		free(node);
-		head = node;
+		node = next_node;
 	}
 }
